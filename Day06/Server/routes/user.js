a4e970b358e90bff4972622529dd017fa7535fda
rohan.paramane <rohan.paramane@sunbeaminfo.com>
2025-12-18 13:13:17 +0530

    Day06 Data Added

---
+++ Day06/Server/routes/user.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const cryptojs = require('crypto-js')
+const pool = require('../db/pool')
+const result = require('../utils/result')
+
+const router = express.Router()
+
+router.post('/signup', (req, res) => {
+    const { name, email, password, mobile } = req.body
+    const sql = `INSERT INTO users(name,email,password,mobile) VALUES (?,?,?,?)`
+    const hashedPassword = cryptojs.SHA256(password).toString()
+    pool.query(sql, [name, email, hashedPassword, mobile], (error, data) => {
+        res.send(result.createResult(error, data))
+    })
+})
+
+router.post('/signin', (req, res) => {
+    const { email, password } = req.body
+    const hashedPassword = cryptojs.SHA256(password).toString()
+    const sql = `SELECT * FROM users WHERE email = ? AND password = ?`
+    pool.query(sql, [email, hashedPassword], (error, data) => {
+        if (error)
+            res.send(result.createResult(error))
+        else if (data.length == 0)
+            res.send(result.createResult("Invalid email or password"))
+        else {
+            // JWT
+            res.send(result.createResult(null, data))
+        }
+    })
+})
+
+// query paramaters
+router.get('/', (req, res) => {
+    const { email } = req.query
+    const sql = `SELECT * FROM users WHERE email = ?`
+    pool.query(sql, [email], (error, data) => {
+        res.send(result.createResult(error, data))
+    })
+})
+
+// request parameters
+router.delete('/:uid', (req, res) => {
+    const uid = req.params.uid
+    const sql = `DELETE FROM users WHERE uid = ?`
+    pool.query(sql, [uid], (error, data) => {
+        res.send(result.createResult(error, data))
+    })
+})
+
+module.exports = router
---
+++ Day06/Server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const userRouter = require('./routes/user')
+
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+    // for ever incoming request this middleware will be called
+    const path = req.url
+    if (path == '/user/signin' || path == '/user/signup')
+        next()
+    else {
+        res.send('You need to be authorized')
+    }
+})
+app.use('/user', userRouter)
+
+app.listen(4000, 'localhost', () => {
+    console.log('Server is running on 4000');
+});
